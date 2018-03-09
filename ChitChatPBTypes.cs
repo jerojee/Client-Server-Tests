11a67aeb85c0866e0bff0f0aee480a0c532c7d3e
ojeremiah <35983205+ojeremiah@users.noreply.github.com>
2018-03-08 17:40:19 -0800

    Client using Protobuf-net

---
+++ ChitChatPBTypes.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProtoBuf;
+
+namespace ChitChat.Events
+{
+    public enum EventType
+    {
+        NEW_USER,
+        LOGIN,
+        USER_INFO,
+        CREATE_CHAT, //JEREMY: Adding new types
+        SEND_MESSAGE, // new
+        NONE,
+    }
+
+    [ProtoContract]
+    [ProtoInclude(100, typeof(UserInfo))]
+    [ProtoInclude(101, typeof(Login))]
+    [ProtoInclude(102, typeof(NewUser))]
+    [ProtoInclude(103, typeof(CreateChat))]
+    [ProtoInclude(104, typeof(SendMessage))]
+    class BaseEvent
+    {
+        [ProtoMember(1)]
+        public EventType Type { get; set; }
+    }
+
+    [ProtoContract]
+    class UserInfo : BaseEvent
+    {
+        [ProtoMember(1)]
+        public string Name { get; set; }
+
+        [ProtoMember(2)]
+        public int ID { get; set; }
+
+        [ProtoMember(3)]
+        public string PhoneNumber { get; set; }
+
+        [ProtoMember(4)]
+        public string Password { get; set; }
+    }
+
+    //JEREMY: Adding proto structs
+    [ProtoContract]
+    class Login : BaseEvent
+    {
+        [ProtoMember(1)]
+        public string Username { get; set; }
+
+        [ProtoMember(2)]
+        public string Password { get; set; }
+    }
+
+    [ProtoContract]
+    class NewUser : BaseEvent
+    {
+        [ProtoMember(1)]
+        public string FirstName { get; set; }
+
+        [ProtoMember(2)]
+        public string LastName { get; set; }
+
+        [ProtoMember(3)]
+        public string PhoneNumber { get; set; }
+
+        [ProtoMember(4)]
+        public string Password { get; set; }
+    }
+
+    [ProtoContract]
+    class CreateChat : BaseEvent
+    {
+        [ProtoMember(1)]
+        public string ChatName { get; set; }
+
+        //Possibly add member for a list of users?
+    }
+
+    [ProtoContract]
+    class SendMessage : BaseEvent
+    {
+        [ProtoMember(1)]
+        public string Message { get; set; }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using ProtoBuf;
+using ChitChat.Events;
+
+namespace Test_Client1
+{
+    class Client
+    {
+        void PrintUserInfo(UserInfo user, Login userLogin)
+        {
+            Console.WriteLine($"Name: {user.Name}");
+            Console.WriteLine($"ID: {user.ID}");
+            Console.WriteLine($"Password: {user.Password}");
+            Console.WriteLine($"Event Type: {user.Type.ToString()}");
+
+            Console.WriteLine($"User: {userLogin.Username}");
+            Console.WriteLine($"User: {userLogin.Password}");
+        }
+
+       void PrintMessageToClient(SendMessage m)
+        {
+            Console.WriteLine($"Recieved Message: {m.Message}");
+        }
+
+        public void StartClient()
+        {
+            Console.WriteLine("Starting Client....");
+
+            UserInfo user = new UserInfo
+            {
+                Name = "Jeremy",
+                ID = 33690,
+                PhoneNumber = "111-111-1111",
+                Password = "Password",
+                Type = EventType.USER_INFO,
+            };
+
+            Login userLogin = new Login
+            {
+                Username = "xkrunchy",
+                Password = "Mastersword!!64",
+                Type = EventType.LOGIN,
+            };
+
+
+            Console.WriteLine("Create new user with info: \n");
+            PrintUserInfo(user, userLogin);
+
+            TcpClient _client = null;
+            NetworkStream stream;
+            
+            try
+            {
+
+                _client = new TcpClient("144.37.220.39", 27015);
+
+                stream = _client.GetStream();
+
+                string answer;
+                string newMessageToBeSent;
+
+                BaseEvent incomingEvent = new BaseEvent
+                {
+                    Type = EventType.NONE
+                };
+
+                while (true)
+                {                    
+                    //Sending a new message code below
+                    Console.WriteLine("Do you want to send a message? yes/no?");
+                    answer = Console.ReadLine();
+                    if (answer == "yes")
+                    {
+                        Console.WriteLine("Enter message to be sent: ");
+                        newMessageToBeSent = Console.ReadLine();
+                        SendMessage message = new SendMessage
+                        {
+                            Message = newMessageToBeSent,
+                            Type = EventType.SEND_MESSAGE
+                        };
+
+
+                        
+                                                    
+                            Serializer.SerializeWithLengthPrefix(stream, message, PrefixStyle.Base128);
+                            Console.WriteLine("Sent a {0} event to server...", message.Type.ToString());                          
+                        
+
+                        Console.ReadKey();
+                    }
+                       // BaseEvent incomingEvent = new BaseEvent(); //create to hold incoming event type
+                            
+                                                 
+                       incomingEvent = Serializer.DeserializeWithLengthPrefix<BaseEvent>(stream, PrefixStyle.Base128);   
+                        
+                    
+                        switch (incomingEvent.Type)
+                        {
+                            case EventType.NONE:
+                                Console.WriteLine("No event yet");
+                                break;
+                            case EventType.SEND_MESSAGE:
+                                SendMessage m = (SendMessage)incomingEvent;
+                                Console.WriteLine("Event was of type: {0}", incomingEvent.Type.ToString());
+                                PrintMessageToClient(m);
+                                break;
+                            default:
+                                break;
+                        }                   
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Socket exception: {0}", e.ToString());
+            }
+            finally
+            {
+                _client?.Close();
+            }
+        }
+
+
+        static int Main(String[] args)
+        {
+            Client c = new Client { };
+
+            c.StartClient();
+
+            return (0);
+        }
+    }
+}
+
